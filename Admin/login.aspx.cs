ce7d20e217ea3b1219731b2199192528324e6390
janvi2013 <knpsharma1987@gmail.com>
2019-05-18 20:57:09 +0530

    find this latest code uploaded on 18/05/2019 at 8:56 pm
    find this latest code uploaded on 18/05/2019 at 8:56 pm

---
+++ Admin/login.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class Admin_login : System.Web.UI.Page
+{
+    String username;
+    String password;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+
+    }
+    public void Login()
+    {
+        if(txtusername.Text=="admin" && txtpassword.Text=="admin123")
+        {
+            Response.Redirect("~/Admin/welcomeAdmin.aspx");
+        }
+    }
+
+    protected void btnlogin_Click(object sender, EventArgs e)
+    {
+        Login();
+    }
+}
---
+++ Admin/welcomeAdmin.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/admin.master" AutoEventWireup="true" CodeFile="welcomeAdmin.aspx.cs" Inherits="Admin_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <p style="color:red">welcome admin page welcome admin page welcome admin page welcome admin page welcome admin pagewelcome admin page welcome admin page welcome admin page welcome admin page  </p>
+</asp:Content>
+
