b004fe8be11b47e75f4859ba5eb9fd10661c5125
Ruchit <mehta.ruchit28@gmail.com>
2019-06-23 23:24:38 +0530

    Remove html from description in FB sharing

--- post-detail.aspx.cs
+++ post-detail.aspx.cs
@@ -44,7 +44,7 @@ public partial class _Default : System.Web.UI.Page
         Title.InnerText = dt.Rows[0]["Title"].ToString();
 
         HtmlGenericControl MetaDescription = ((HtmlGenericControl)this.Page.Master.FindControl("MetaForDescription"));
-        MetaDescription.Attributes.Add("content",dt.Rows[0]["Description"].ToString());
+        MetaDescription.Attributes.Add("content", HttpUtility.HtmlDecode( dt.Rows[0]["DescText"].ToString()));
 
         HtmlGenericControl MetaImage = ((HtmlGenericControl)this.Page.Master.FindControl("MetaForImage"));
         string pattern = Regex.Match(dt.Rows[0]["Thumbnailimage"].ToString(), "<img.+?src=[\"'](.+?)[\"'].+?>", RegexOptions.IgnoreCase).Groups[1].Value;
